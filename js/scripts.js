73e3347869bf0d16cfd7789a408210cb35255503
Praba Ponnambalam <praba@codechilli.lk>
2024-01-27 10:59:20 +0530

    🎉 init
    first commit

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Empty JS for your own code to be here
+
+ $('.btnNext').click(function(){
+  $('.nav-tabs > .active').next('li').find('a').trigger('click');
+});
+
+  $('.btnPrevious').click(function(){
+  $('.nav-tabs > .active').prev('li').find('a').trigger('click');
+});
